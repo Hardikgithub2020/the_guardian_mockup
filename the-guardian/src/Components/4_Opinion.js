3e4a69736e1bf89e44d6b4fc0257ea2890ba800a
Hardikgithub2020 <hardik.unity@gmail.com>
2020-08-07 01:57:16 -0400

    Finished opinion Component

--- the-guardian/src/Components/4_Opinion.js
+++ the-guardian/src/Components/4_Opinion.js
@@ -1,17 +1,113 @@
 import React from 'react';
-import '../App.css'
+import '../App.css';
+import './4_Opinion.css';
 
 function Opinion (props) {
     return (
-        <div className='main'>
-            <aside>
-                <h6>Opinion</h6>
-            </aside>
-            <section>
-                <h6>Section</h6>
-            </section>
+        <div className="main">
+          <aside>
+            <h6>opinion</h6>
+          </aside>
+          <section id="opinionSection">
+            <div id="opinionSection1">
+            <div>
+                <img
+                  src={require("./images/1208.jpg")}
+                  style={{ width: "100%", height: "90%" }}
+                />
+              </div>
+              <div>
+                <h5>
+                  <span>
+                    //
+                  </span>No, Clint Eastwood, female journalists don't trade sex for information
+                </h5>
+                <h5><span>Ankita Rao</span></h5>
+                
+              </div>
+              
+            </div>
+            <div id="opinionSection2">
+              <div>
+                <img
+                  src={require("./images/300.jpg")}
+                  style={{ width: "100%", height: "70px" }}
+                />
+                <div>
+                  <h5>
+                    <span>Best of 2019 ////</span>
+                  </h5>
+                  <h5>
+                    White people assume niceness is the answer to racial inequality.
+                    It's not
+                  </h5>
+                </div>
+              </div>
+            </div>
+            <div id="opinionSection3">
+              <div id="opinionSection3Child">
+                <img
+                  src={require("./images/4592.jpg")}
+                  style={{ width: "100%", height: "50px",margin : 5 } }
+                />
+    
+                <p>
+                  <span>Mind Your own your businesss, Alexa/ </span>How to keep sceret from your voice
+                </p>
+                
+              </div>
+              <div id="opinionSection3Child">
+                <img
+                  src={require("./images/5000.jpg")}
+                  style={{ width: "100%", height: "50px",margin : 5 } }
+                />
+    
+                <p>
+                  <span>Mind Your own your businesss, Alexa/ </span>How to keep sceret from your voice
+                </p>
+              </div>
+              <div id="opinionSection3Child">
+                <img
+                  src={require("./images/5004.jpg")}
+                  style={{ width: "100%", height: "50px",margin : 5 } }
+                />
+    
+                <p>
+                  <span>Mind Your own your businesss, Alexa/ </span>How to keep sceret from your voice
+                </p>
+              </div>
+              <div id="opinionSection3Child">
+                <img
+                  src={require("./images/4592.jpg")}
+                  style={{ width: "100%", height: "50px",margin : 5 } }
+                />
+    
+                <p>
+                  <span>Mind Your own your businesss, Alexa/ </span>How to keep sceret from your voice
+                </p>
+              </div>
+            </div>
+            <div id="opinionSection4">
+            <div>
+                <img
+                  src={require("./images/2048.jpg")}
+                  style={{ width: "100%", height: "70px" }}
+                />
+                <div>
+                  <h5>
+                    <span>Best of 2019 ////</span>
+                  </h5>
+                  <h5>
+                    White people assume niceness is the answer to racial inequality.
+                    It's not
+                  </h5>
+                </div>
+              </div>
+                
+            </div>
+          </section>
         </div>
-    );
+      );
 }
 
 export default Opinion;
